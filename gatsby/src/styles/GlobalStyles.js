889ec19190a49b62f37282d1fc3cb1d1cd3adb30
holjeffcomb <72577303+holjeffcomb@users.noreply.github.com>
2022-09-06 19:15:51 -0700

    fix global styles

--- gatsby/src/styles/GlobalStyles.js
+++ gatsby/src/styles/GlobalStyles.js
@@ -1,45 +1,86 @@
-import { createGlobalStyle } from 'styled-components';
-import font from '../assets/fonts/BeginningYoga.ttf';
+import { createGlobalStyle } from "styled-components";
+import bg from "../assets/images/bg.svg";
+import stripes from "../assets/images/stripes.svg";
 
-const Typography = createGlobalStyle`
-  @font-face {
-    font-family: BeginningYoga;
-    src: url(${font});
+const GlobalStyles = createGlobalStyle`
+  :root {
+    --red: #FF4949;
+    --black: #2E2E2E;
+    --yellow: #ffc600;
+    --white: #fff;
+    --grey: #efefef;
   }
   html {
-    font-family: BeginningYoga, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    color: var(--black);
-  }
-  p, li {
-    font-family: Europa;
-    letter-spacing: 0.5px;
-    font-size: 1rem;
-  }
-  h1,h2,h3,h4,h5,h6 {
-    font-weight: normal;
-    margin: 0;
-  }
-  a {
-    color: var(--black);
-    text-decoration-color: var(--red);
-    /* Chrome renders this weird with this font, so we turn it off */
-    text-decoration-skip-ink: none;
-  }
-  mark, .mark {
-    background: var(--yellow);
-    padding: 0 2px 2px 2px;
-    margin: 0;
-    display: inline;
-    line-height: 1;
+    background-image: url(${bg});
+    background-size: 450px;
+    background-attachment: fixed;
+    font-size: 10px;
   }
 
-  .center {
-    text-align: center;
+  body {
+    font-size: 2rem;
+  }
+
+  fieldset {
+    border-color: rgba(0,0,0,0.1);
+    border-width: 1px;
+  }
+
+  button {
+    background: var(--red);
+    color: white;
+    border: 0;
+    padding: 0.6rem 1rem;
+    border-radius: 2px;
+    cursor: pointer;
+    --cast: 2px;
+    box-shadow: var(--cast) var(--cast) 0 var(--grey);
+    text-shadow: 0.5px 0.5px 0 rgba(0,0,0,0.2);
+    transition: all 0.2s;
+    &:hover {
+      --cast: 4px;
+    }
+  }
+
+  .gatsby-image-wrapper img[src*=base64\\,] {
+    image-rendering: -moz-crisp-edges;
+    image-rendering: pixelated;
+  }
+
+  /* Scrollbar Styles */
+  body::-webkit-scrollbar {
+    width: 12px;
+  }
+  html {
+    scrollbar-width: thin;
+    scrollbar-color: var(--red) var(--white);
+  }
+  body::-webkit-scrollbar-track {
+    background: var(--white);
+  }
+  body::-webkit-scrollbar-thumb {
+    background-color: var(--red) ;
+    border-radius: 6px;
+    border: 3px solid var(--white);
+  }
+
+  hr {
+    border: 0;
+    height: 8px;
+    background-image: url(${stripes});
+    background-size: 1500px;
+  }
+
+  img {
+    max-width: 100%;
   }
 
   .tilt {
     transform: rotate(-2deg);
+    position: relative;
+    display: inline-block;
   }
+
 `;
 
-export default Typography;
+export default GlobalStyles;
