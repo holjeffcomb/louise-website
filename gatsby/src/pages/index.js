e0bac750c863f7ce7afc3a31a7951b6c4549df40
holjeffcomb <72577303+holjeffcomb@users.noreply.github.com>
2022-09-06 18:35:52 -0700

    first commit

---
+++ gatsby/src/pages/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function HomePage() {
+  return <p>Hey i'm the home page :)</p>;
+}
---
+++ gatsby/src/pages/slicemasters.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function SliceMastersPage() {
+  return <p>Slice Masters</p>;
+}
