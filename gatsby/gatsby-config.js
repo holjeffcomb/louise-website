9788f2d0fdcea86101c3b6c73bfb2528e86086b2
holjeffcomb <72577303+holjeffcomb@users.noreply.github.com>
2022-09-11 14:24:26 -0700

    integrate with sanity

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Louise Holcomb's Webpage`,
+    siteUrl: `https://www.louiseholcomb.com`,
+    description: `Website of Louise Holcomb`,
   },
   plugins: [
     'gatsby-plugin-styled-components',
@@ -20,6 +22,16 @@ module.exports = {
         path: './src/images/',
       },
       __key: 'images',
+    },
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'aphbxqqx',
+        dataset: 'production',
+        watchMode: true,
+        // TODO: move token to different file
+        token: "skO2GUy5ftIIjL2qnJDwgl69dH97AjHESyND9EpITeAzfjbGDc725ZIMBuxuNCY7uuSRMICEXSlE8qLNeEsWVhlOlgRPow3ZZ1KsC8GE2dOlgF2w2g7HsXlqF9h1AqL8u3jwG7sxx2aT5gHcQrqG5XJgpr92CPRSjW4fJSSdugVXihNeUwN3",
+      }
     },
   ],
 };
