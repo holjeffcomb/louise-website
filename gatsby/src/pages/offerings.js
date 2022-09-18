52a0e60f35a950756c16a54d63842478f55d49bd
holjeffcomb <72577303+holjeffcomb@users.noreply.github.com>
2022-09-18 13:01:38 -0700

    update sanity interactions

--- gatsby/src/pages/offerings.js
+++ gatsby/src/pages/offerings.js
@@ -1,5 +1,31 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-export default function ContactPage() {
+export default function OfferingsPage() {
   return <p>Offerings</p>;
 }
+
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
